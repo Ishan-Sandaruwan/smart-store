2b8ce30877cb0b82ba827cd08a1d49432bfd0ca5
Ishan-Sandaruwan <sadaruwan0427@gmail.com>
2024-07-19 12:49:32 +0530

    create user model

---
+++ api/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  mobile: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ['customer', 'admin', 'cashier'],
+    required: true,
+    default: 'customer',
+  },
+  address: String,
+  loyaltyPoints: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
